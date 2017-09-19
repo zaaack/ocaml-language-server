59e621b4021c706d0b1f5ef6be0e4aa2e1688494
Javier Chávarri <javier.chavarri@gmail.com>
2017-09-19 04:35:23 +0200

    Char index fixes

--- src/shared/parser/bucklescript/index.ts
+++ src/shared/parser/bucklescript/index.ts
@@ -54,9 +54,9 @@ export function parseTypeErrors(bsbOutput: string): { [key: string]: types.Diagn
     const fileUri = "file://" + errorMatch[1];
     // Suppose most complex case, path/to/file.re 10:20-15:5 message
     const startLine = Number(errorMatch[2]) - 1;
-    const startCharacter = Number(errorMatch[3]);
+    const startCharacter = Number(errorMatch[3]) - 1;
     let endLine = Number(errorMatch[4]) - 1;
-    let endCharacter = Number(errorMatch[5]);
+    let endCharacter = Number(errorMatch[5]); // Non inclusive originally
     const message = errorMatch[6].replace(/\n  /g, "\n");
     if (isNaN(endLine)) {
       // Format path/to/file.re 10:20 message
@@ -64,7 +64,7 @@ export function parseTypeErrors(bsbOutput: string): { [key: string]: types.Diagn
       endLine = startLine;
     } else if (isNaN(endCharacter)) {
       // Format path/to/file.re 10:20-15 message
-      endCharacter = endLine; // Format is L:SC-EC
+      endCharacter = endLine + 1; // Format is L:SC-EC
       endLine = startLine;
     }
 
