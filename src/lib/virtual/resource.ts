d1d9a38bca3d9d68d7649983c8ebf2faa37e2b31
Darin Morrison <freebroccolo@users.noreply.github.com>
2017-10-21 19:15:50 -0600

    Minimize string conversion

--- src/lib/virtual/resource.ts
+++ src/lib/virtual/resource.ts
@@ -22,40 +22,40 @@ export class Resource {
         // FIXME: move this check somewhere earlier and do it only once
         const localappdata = process.env.localappdata;
         if (null == localappdata) throw new Error("LOCALAPPDATA must be set in environment to interpret WSL /home");
-        let uri = this.uri;
+        let uri = this.uri.toString(skipEncoding);
         let searcher: RegExp;
         let match: RegExpMatchArray | null = null;
         // rewrite /mnt/…
         searcher = /^file:\/\/\/mnt\/([a-zA-Z])\/(.*)$/;
-        if (match = uri.toString(skipEncoding).match(searcher)) {
+        if (match = uri.match(searcher)) {
           match.shift();
           const drive = match.shift() as string;
           const  rest = match.shift() as string;
-          uri = URI.parse(`file:///${drive}:/${rest}`);
+          uri = `file:///${drive}:/${rest}`;
         }
         // rewrite /home/…
         searcher = /^file:\/\/\/home\/(.+)$/;
-        if (match = uri.toString(skipEncoding).match(searcher)) {
+        if (match = uri.match(searcher)) {
           match.shift();
           const rest = match.shift() as string;
-          uri = URI.parse(`${URI.file(localappdata).toString(skipEncoding)}/lxss/home/${rest}`);
+          uri = `${URI.file(localappdata).toString(skipEncoding)}/lxss/home/${rest}`;
         }
-        return uri;
+        return URI.parse(uri);
     }
   }
   protected readWSL(skipEncoding: boolean): URI {
     switch (this.source) {
       case Host.Native:
-        let uri = this.uri;
+        let uri = this.uri.toString(skipEncoding);
         let searcher: RegExp;
         let match: RegExpMatchArray | null = null;
         searcher = /^file:\/\/\/([a-zA-Z]):\//;
-        if (match = uri.toString(skipEncoding).match(searcher)) {
+        if (match = uri.match(searcher)) {
           match.shift();
           const drive = match.shift() as string;
-          uri = URI.parse(`file:///mnt/${drive}/`);
+          uri = `file:///mnt/${drive}/`;
         }
-        return uri;
+        return URI.parse(uri);
       case Host.WSL:
         return this.uri;
     }
