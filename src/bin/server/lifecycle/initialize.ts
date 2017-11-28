e5d7b6ad19978630ec1a8f2389c7195136db1714
Darin Morrison <freebroccolo@users.noreply.github.com>
2017-11-27 20:09:05 -0700

    WIP: restart merlin automatically

--- src/bin/server/lifecycle/initialize.ts
+++ src/bin/server/lifecycle/initialize.ts
@@ -1,5 +1,5 @@
 import * as server from "vscode-languageserver";
-import { ISettings, merlin } from "../../../lib";
+import { ISettings } from "../../../lib";
 import capabilities from "../capabilities";
 import Session from "../session";
 
@@ -16,12 +16,6 @@ export default function(
       ? event.initializationOptions
       : ISettings.defaults.reason;
     await session.initialize();
-    const request = merlin.Sync.protocol.version.set(3);
-    const response = await session.merlin.sync(request);
-    if (response.class !== "return" || response.value.selected !== 3) {
-      session.connection.dispose();
-      throw new Error("onInitialize: failed to establish protocol v3");
-    }
     return { capabilities };
   };
 }
--- src/bin/server/processes/merlin.ts
+++ src/bin/server/processes/merlin.ts
@@ -9,11 +9,59 @@ import Session from "../session";
 
 export default class Merlin implements server.Disposable {
   private readonly queue: async.AsyncPriorityQueue<merlin.Task>;
-  private readline: readline.ReadLine;
-  private process: childProcess.ChildProcess;
+  private readonly readline: readline.ReadLine;
+  private readonly process: childProcess.ChildProcess;
 
-  constructor(private readonly session: Session) {
-    this.queue = async.priorityQueue((task, callback) => {
+  constructor(private readonly session: Session) {}
+
+  public dispose(): void {
+    this.readline.close();
+  }
+
+  public async initialize(): Promise<void> {
+    const ocamlmerlin = this.session.settings.reason.path.ocamlmerlin;
+    const cwd = this.session.initConf.rootUri || this.session.initConf.rootPath;
+    const options = cwd ? { cwd: URI.parse(cwd).fsPath } : {};
+
+    (this.process as any) = this.session.environment.spawn(
+      ocamlmerlin,
+      [],
+      options,
+    );
+    // this.process.on("exit", (code, signal) => {
+    //   this.session.connection.console.log(JSON.stringify({ code, signal }));
+    // });
+    this.process.on("error", (error: Error & { code: string }) => {
+      // this.session.connection.console.log(JSON.stringify({ error }));
+      if (error.code === "ENOENT") {
+        this.session.connection.window.showWarningMessage(
+          `Cannot find merlin binary at "${ocamlmerlin}".`,
+        );
+        this.session.connection.window.showWarningMessage(
+          `Double check your path or try configuring "reason.path.ocamlmerlin" under "User Settings".`,
+        );
+        throw error;
+      }
+    });
+    this.process.stderr.on("data", (data: string) => {
+      this.session.connection.window.showErrorMessage(
+        `ocamlmerlin error: ${data}`,
+      );
+    });
+
+    (this.readline as any) = readline.createInterface({
+      input: this.process.stdout,
+      output: this.process.stdin,
+      terminal: false,
+    });
+    // this.readline.on("close", () => {
+    //   this.session.connection.console.log("readline: close");
+    // });
+
+    const worker: async.AsyncWorker<merlin.Task, merlin.MerlinResponse<any>> = (
+      task,
+      callback,
+    ) => {
       const begunProcessing = new Date();
       if (task.token && task.token.isCancellationRequested) {
         return callback({
@@ -23,44 +71,15 @@ export default class Merlin implements server.Disposable {
       }
       this.readline.question(
         JSON.stringify(task.task),
-        _.flow(JSON.parse, this.logMessage(begunProcessing, task), callback),
+        _.flow(JSON.parse, this.logMessage(begunProcessing, task), data => {
+          // this.session.connection.console.log(JSON.stringify(data));
+          return callback(data);
+        }),
       );
-    }, 1);
-  }
+    };
+    (this.queue as any) = async.priorityQueue(worker, 1);
 
-  public dispose(): void {
-    this.readline.close();
-  }
-
-  public initialize(): void {
-    const ocamlmerlin = this.session.settings.reason.path.ocamlmerlin;
-    const cwd = this.session.initConf.rootUri || this.session.initConf.rootPath;
-    const options = cwd ? { cwd: URI.parse(cwd).fsPath } : {};
-    this.process = this.session.environment.spawn(ocamlmerlin, [], options);
-
-    this.process.on("error", (error: Error & { code: string }) => {
-      if (error.code === "ENOENT") {
-        const msg = `Cannot find merlin binary at "${ocamlmerlin}".`;
-        this.session.connection.window.showWarningMessage(msg);
-        this.session.connection.window.showWarningMessage(
-          `Double check your path or try configuring "reason.path.ocamlmerlin" under "User Settings".`,
-        );
-        this.dispose();
-        throw error;
-      }
-    });
-
-    this.process.stderr.on("data", (data: string) => {
-      this.session.connection.window.showErrorMessage(
-        `ocamlmerlin error: ${data}`,
-      );
-    });
-
-    this.readline = readline.createInterface({
-      input: this.process.stdout,
-      output: this.process.stdin,
-      terminal: false,
-    });
+    this.establishProtocol();
   }
 
   public query<I, O>(
@@ -69,6 +88,9 @@ export default class Merlin implements server.Disposable {
     id?: types.TextDocumentIdentifier,
     priority: number = 0,
   ): merlin.Response<O> {
+    // this.session.connection.console.log(
+    //   JSON.stringify({ query, id, priority }),
+    // );
     const context: ["auto", string] | undefined = id
       ? ["auto", URI.parse(id.uri).fsPath]
       : undefined;
@@ -78,11 +100,30 @@ export default class Merlin implements server.Disposable {
     );
   }
 
+  public restart(): void {
+    if (this.queue) {
+      this.queue.kill();
+      (this.queue as any) = null;
+    }
+    if (this.readline) {
+      this.readline.close();
+      (this.readline as any) = null;
+    }
+    if (this.process) {
+      this.process.kill();
+      (this.process as any) = null;
+    }
+    this.initialize();
+  }
+
   public sync<I, O>(
     { sync: query }: merlin.Sync<I, O>,
     id?: types.TextDocumentIdentifier,
     priority: number = 0,
   ): merlin.Response<O> {
+    // this.session.connection.console.log(
+    //   JSON.stringify({ query, id, priority }),
+    // );
     const context: ["auto", string] | undefined = id
       ? ["auto", URI.parse(id.uri).fsPath]
       : undefined;
@@ -92,9 +133,23 @@ export default class Merlin implements server.Disposable {
     );
   }
 
-  private logMessage<A>(begunProcessing: Date, task: merlin.Task) {
-    return (result: A) => {
-      if (this.session.settings.reason.diagnostics.merlinPerfLogging) {
+  private async establishProtocol(): Promise<void> {
+    const request = merlin.Sync.protocol.version.set(3);
+    const response = await this.sync(request);
+    if (response.class !== "return" || response.value.selected !== 3) {
+      throw new Error("onInitialize: failed to establish protocol v3");
+    }
+  }
+
+  private logMessage<A>(
+    begunProcessing: Date,
+    task: merlin.Task,
+  ): (result: A) => A {
+    return result => {
+      if (
+        this.session.settings.reason.diagnostics &&
+        this.session.settings.reason.diagnostics.merlinPerfLogging
+      ) {
         const queueDuration =
           begunProcessing.getTime() - task.enqueuedAt.getTime();
         const merlinDuration = new Date().getTime() - begunProcessing.getTime();
