5a2c0fd2b6cf98ecc2f1d7a02f6dfda6142dbbd8
Javier Chávarri <javier.chavarri@gmail.com>
2017-12-04 00:42:58 +0100

    Re-enable merlin restart (#79)

--- src/bin/server/command/restartMerlin.ts
+++ src/bin/server/command/restartMerlin.ts
@@ -1,14 +1,19 @@
-// import { merlin } from "../../../lib";
+import { merlin } from "../../../lib";
 import Session from "../session";
 
-export default async function(_session: Session): Promise<void> {
-  // await session.merlin.restart();
+export default async function(session: Session): Promise<void> {
+  await session.merlin.restart();
   // // FIXME: put this in a method after refactoring Synchronizer
-  // for (const document of session.synchronizer.documents.values()) {
-  //   const content = document.getText();
-  //   const request = merlin.Sync.tell("start", "end", content);
-  //   await session.merlin.sync(request, document, Infinity);
-  //   await session.analyzer.refreshImmediate(document);
-  //   await session.indexer.populate(document);
-  // }
+  for (const document of session.synchronizer.documents.values()) {
+    const content = document.getText();
+    const request = merlin.Sync.tell("start", "end", content);
+    await session.merlin.sync(request, document, Infinity);
+    const tools: Set<string> = new Set(
+      session.settings.reason.diagnostics.tools,
+    );
+    if (!tools.has("bsb")) {
+      await session.analyzer.refreshImmediate(document);
+    }
+    await session.indexer.populate(document);
+  }
 }
