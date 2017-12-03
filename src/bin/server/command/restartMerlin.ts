a0329e55dedba28469f00b645cc46d326deab2ab
Darin Morrison <freebroccolo@users.noreply.github.com>
2017-12-03 13:10:08 -0700

    Disable merlin auto-restart for now

--- src/bin/server/command/restartMerlin.ts
+++ src/bin/server/command/restartMerlin.ts
@@ -1,14 +1,14 @@
-import { merlin } from "../../../lib";
+// import { merlin } from "../../../lib";
 import Session from "../session";
 
-export default async function(session: Session): Promise<void> {
-  await session.merlin.restart();
-  // FIXME: put this in a method after refactoring Synchronizer
-  for (const document of session.synchronizer.documents.values()) {
-    const content = document.getText();
-    const request = merlin.Sync.tell("start", "end", content);
-    await session.merlin.sync(request, document, Infinity);
-    await session.analyzer.refreshImmediate(document);
-    await session.indexer.populate(document);
-  }
+export default async function(_session: Session): Promise<void> {
+  // await session.merlin.restart();
+  // // FIXME: put this in a method after refactoring Synchronizer
+  // for (const document of session.synchronizer.documents.values()) {
+  //   const content = document.getText();
+  //   const request = merlin.Sync.tell("start", "end", content);
+  //   await session.merlin.sync(request, document, Infinity);
+  //   await session.analyzer.refreshImmediate(document);
+  //   await session.indexer.populate(document);
+  // }
 }
